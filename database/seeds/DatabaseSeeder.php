1ce7ec3366ffaa427b2db04be34e0283aa34f318
Nelabh Kotiya <nelabhkotiya@gmail.com>
2016-10-15 03:34:20 +0530

    Admin DB Seeder Added

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\User;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +12,11 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+    	$user = new User;
+    	$user->name = "Admin";
+    	$user->email = "admin@gmail.com";
+    	$user->password = Hash::make('helloworld');
+    	$user->level = 5;
+    	$user->save();
     }
 }
